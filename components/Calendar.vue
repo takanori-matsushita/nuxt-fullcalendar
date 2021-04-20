b9049343571dedf6d3cd5926de36b0ef0cec3b82
takanori-matsushita <t.matsushita@arcjobz.com>
2021-04-21 00:52:01 +0900

    [add]fullcalendar component plugin

---
+++ components/Calendar.vue
@@ -0,0 +1,29 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+export default {
+  components: {
+    FullCalendar,
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [timeGridPlugin, interactionPlugin],
+        // initialView: 'timeGridWeek',
+      },
+    }
+  },
+}
+</script>
+
+<style>
+/* @import '~/node_modules/@fullcalendar/common/main.css'; */
+/* @import '~/node_modules/@fullcalendar/daygrid/main.css'; */
+/* @import '~/node_modules/@fullcalendar/timegrid/main.css'; */
+</style>
---
+++ plugins/full-calendar.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import Calendar from '~/components/Calendar'
+Vue.component('full-calendar', Calendar)

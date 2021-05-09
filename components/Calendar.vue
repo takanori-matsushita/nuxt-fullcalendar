0b22b36149e5696e3cca0aeee647b2b4e77fa5fd
takanori-matsushita <t.matsushita@arcjobz.com>
2021-05-09 22:56:11 +0900

    [fix]weekday insert

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -27,7 +27,11 @@ export default {
         },
         scrollTime: '0:00:00',
         titleFormat: { year: 'numeric', month: 'short' },
-        dayHeaderFormat: { day: 'numeric', weekday: 'short' },
+        // dayHeaderFormat: { day: 'numeric' },
+        dayHeaderFormat(date) {
+          const day = date.date.day
+          return day
+        },
         allDaySlot: false, // 終日を非表示
         slotLabelFormat: { hour: 'numeric', minute: '2-digit' },
         // eventColor: '#ff00ff', // イベントの色を変える
@@ -39,7 +43,7 @@ export default {
         eventBorderColor: '#FF525C',
         eventTextColor: '#FF525C',
         initialEvents: [
-          { title: 'nice event', start: new Date() },
+          { title: 'nice event', start: new Date('2021-5-4 10:00:00') },
           {
             title: 'Click for Google',
             url: 'http://google.com/',
@@ -49,10 +53,40 @@ export default {
       },
     }
   },
+  mounted() {
+    this.pressChangeWeek()
+    this.addWeekday()
+  },
+  methods: {
+    pressChangeWeek() {
+      const buttons = document.querySelectorAll(
+        '.fc-today-button, [aria-label="prev"], [aria-label="next"]'
+      )
+      for (const btn of buttons) {
+        btn.addEventListener('click', () => {
+          this.addWeekday()
+        })
+      }
+    },
+    addWeekday() {
+      const weekdays = document.getElementsByClassName('fc-col-header-cell')
+      const weekdaysText = ['日', '月', '火', '水', '木', '金', '土']
+      for (let i = 0; i <= 6; i++) {
+        const el = document.createElement('span')
+        el.className = 'weekday'
+        if (weekdays[i].classList.contains('fc-day-today')) {
+          el.classList.add('weekday--today')
+        }
+        el.textContent = weekdaysText[i]
+        weekdays[i].prepend(el)
+      }
+    },
+  },
 }
 </script>
 
 <style>
+/* toolbar style */
 .fc .fc-today-button,
 .fc .fc-today-button:disabled {
   border: 1px solid #333333;
@@ -89,5 +123,65 @@ export default {
 
 .fc .fc-timegrid-slot {
   height: 40px;
+}
+
+.fc-col-header-cell {
+  border-left: none !important;
+  border-right: none !important;
+}
+
+.fc .fc-scrollgrid-section-header > * {
+  border: none;
+}
+
+.fc .fc-scrollgrid-section > * {
+  border-bottom: none;
+}
+
+.fc-timegrid-axis,
+.fc-theme-standard .fc-scrollgrid,
+.fc .fc-timegrid-slot-label {
+  border: none !important;
+}
+
+.fc .fc-day {
+  font-size: 22px;
+}
+
+.fc .fc-day-today {
+  position: relative;
+  color: #fff;
+  /* background: #ff525c; */
+}
+
+.fc-col-header,
+.fc-col-header-cell {
+  height: 50px !important;
+}
+
+.weekday {
+  color: rgba(0, 0, 0, 0.3);
+  font-size: 10px;
+}
+
+.weekday--today {
+  color: #ff525c;
+}
+
+.fc-scrollgrid-sync-inner {
+  margin-bottom: 10px;
+}
+
+.fc-col-header-cell.fc-day-today:before {
+  content: '';
+  position: absolute;
+  bottom: 11px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 35px;
+  height: 35px;
+  background: #ff525c;
+  border-radius: 50%;
+  z-index: -1;
 }
 </style>
